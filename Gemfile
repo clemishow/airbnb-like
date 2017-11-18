47801673ef349e4fc7747598651a34db6bbc3654
clemishow <clement.saulnier@hetic.net>
2017-11-18 22:05:21 +0100

    [ADD] Gem devise with user sign up, log in, log out, profile editing, destroy

--- Gemfile
+++ Gemfile
@@ -22,7 +22,10 @@ gem 'uglifier', '>= 1.3.0'
 # Image uploader
 gem 'carrierwave', '~> 1.0.0'
 gem 'mini_magick', '~> 4.3'
-gem "fog-aws" # AWS S3
+gem 'fog-aws' # AWS S3
+
+# Users manager
+gem 'devise'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :account_update_permitted_parameters, :sign_in_permitted_parameters, if: :devise_controller?
+
+  protected
+    def account_update_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update) do |user_params|
+        user_params.permit(:firstname, :lastname, :current_password, :email, :password, :password_confirmation)
+      end
+    end
+
+  protected
+    def sign_in_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:firstname, :lastname, :email, :password, :password_confirmation)
+      end
+    end
 end
