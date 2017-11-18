3d644519468c1f4039063c88a1b3b7f1ac22ab18
clemishow <clement.saulnier@hetic.net>
2017-11-18 22:16:54 +0100

    [ADD] Email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,21 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Confirm email 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'airbnb-like-pg.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.gmail.com',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['AIRBNB_USER'],
+      :password       => ENV['AIRBNB_PASSWORD'],
+      :domain         => 'gmail.com',
+      :enable_starttls_auto => true
+  }
+
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
