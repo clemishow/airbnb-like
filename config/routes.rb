8cc58efad3c8639da36610a56ccac8d3e5ebefa1
clemishow <clement.saulnier@hetic.net>
2017-12-15 17:37:18 +0100

    [ADD] Handle 404 and 500 error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   devise_for :admins
   devise_for :users
 
@@ -22,6 +26,9 @@ Rails.application.routes.draw do
   namespace :users do
     resources :workshops
   end
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   root 'workshops#index', as: 'home'
 end
