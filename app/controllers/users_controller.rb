c57345041d3a24c59c852f0426d2140c547e4545
clemishow <clement.saulnier@hetic.net>
2017-12-15 16:35:45 +0100

    [ADD] Profile page + errors message on login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
+  def show 
+    @user = User.find_by(id: current_user.id)
+    puts current_user.id.inspect
+  end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -34,6 +34,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :medium do
+    process resize_to_fit: [350, 350]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
