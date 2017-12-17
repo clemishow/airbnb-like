798b1bb105ffb3bce623cb5765b7b3785a6d0fde
clemishow <clement.saulnier@hetic.net>
2017-12-17 18:16:55 +0100

    [ADD] Title on errors page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
+    @title = 'Page not found'
     render(:status => 404)
   end
 
   def internal_server_error
+    @title = 'Internal error'
     render(:status => 500)
   end
 end
