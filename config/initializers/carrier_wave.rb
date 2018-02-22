0a39d3d66bcb7b94508f4b650c4a03329ff22b24
Saulnier <clement.saulnier@hetic.net>
2018-02-22 09:47:43 +0100

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,10 +4,10 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                   
   config.fog_credentials = {
-    provider:              'AWS',                   
-    aws_access_key_id:     'AKIAJ4MCHDLTVTSOCSBQ',                   
-    aws_secret_access_key: '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',
-    region:                'eu-central-1'
+    provider:              '',                   
+    aws_access_key_id:     '',                   
+    aws_secret_access_key: '',
+    region:                ''
   }
   config.fog_directory  = 'airbnb-like-pg'
 end
